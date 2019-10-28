33615fda925723f9d89d6bc493aa933002ca0a98
dragonman225 <zxcvb22217@gmail.com>
2019-10-29 00:45:38 +0800

    doc: Update example code

--- examples/hello.js
+++ examples/hello.js
@@ -1,6 +1,9 @@
-const logger = require('../lib');
+const { Logger } = require('../lib')
 
-logger.i('I\'m INFO.');
-logger.e('I\'m ERROR.');
-logger.v('I\'m VERBOSE.');
-logger.v('I log an object.', { id: 12345, name: 'Bob' });
+const log = new Logger('HelloApp', { logLevel: 'verbose', useColor: true })
+
+log.verbose('Something verbose', 'Second verbose message')
+log.debug('Something for debugging')
+log.info('Some useful info')
+log.warn('Some warnings')
+log.error('Something goes wrong')
